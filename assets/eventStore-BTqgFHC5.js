256f0aeb7d5d3521ce7652b4bf72edb3400b09c1
zyh1587409007 <zyh1587409007@users.noreply.github.com>
2024-10-10 02:28:01 +0000

    deploy: d901c42aa65f35ddbaab551efcf02c13cbd2325d

---
+++ assets/eventStore-BTqgFHC5.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-DVeBJqPn.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvents(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-BTqgFHC5.js.map
