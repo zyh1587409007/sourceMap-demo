d901c42aa65f35ddbaab551efcf02c13cbd2325d
love <1587409007@qq.com>
2024-10-10 10:27:11 +0800

    feat: 更新rrweb并部署

--- src/App.vue
+++ src/App.vue
@@ -11,79 +11,81 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/trigger">触发js异常</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <header>
+        <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/trigger">触发js异常</RouterLink>
+                <RouterLink to="/record">录制</RouterLink>
+                <RouterLink to="/play">回放</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-  <RouterView />
+    <RouterView />
 </template>
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    line-height: 1.5;
+    max-height: 100vh;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+    color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+    background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+    border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  nav {
-    font-size: 1rem;
-    /* text-align: left;
+    nav {
+        font-size: 1rem;
+        /* text-align: left;
     margin-left: -1rem;
 
     padding: 1rem 0;
     margin-top: 1rem; */
-  }
+    }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
-// import { findCodeBySourceMap } from './utils/index'
+import { findCodeBySourceMap } from './utils/index'
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(createPinia())
@@ -22,7 +22,7 @@ app.config.errorHandler = (err: any, vm) => {
     }
     // vm!.$message.error(`您触发了一个${err.name} 错误`)
     localStorage.setItem('jsErrorList', JSON.stringify(jsError))
-    //   findCodeBySourceMap(errorStack[0])
+      findCodeBySourceMap(errorStack[0])
     //   console.log('stack', errorStack)
     //   console.log(err, 'error')
     //   console.error(err, 'error')
